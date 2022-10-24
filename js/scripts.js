bbb6b69725f108d488002263d02eade7d65e82c8
delguanzon <yodelguanzon@gmail.com>
2022-10-24 10:41:47 -0700

    Store input field values into one array
    Co-authored-by: Celeste Grobey <celestegrobey@gmail.com>
    Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-window.addEventListener("load", function () {
-
-  
-
+window.addEventListener("load", function() {
+  let form = document.getElementById("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let favoriteThings = [document.getElementById("text1").value, document.getElementById("text2").value, document.getElementById("text3").value,document.getElementById("text4").value];
+    console.log(favoriteThings);
+  });
 });
