bc3f699ef0ef5dae7868b54487a54456b7679da5
delguanzon <yodelguanzon@gmail.com>
2022-10-24 11:19:52 -0700

    Add array elements as li values to html
    Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>
    Co-authored-by: Celeste Grobey <celestegrobey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@ window.addEventListener("load", function() {
     event.preventDefault();
     let favoriteThings = [document.getElementById("text1").value, document.getElementById("text2").value, document.getElementById("text3").value,document.getElementById("text4").value];
     console.log(favoriteThings);
+    console.log("Second: " + favoriteThings[1] + ", First: " + favoriteThings[0] + ", Third: " + favoriteThings[2]);
+    let favoriteThings2 = [];
+    favoriteThings2.push(favoriteThings[1], favoriteThings[0], favoriteThings[2]);
+    // [second element, first element, third element]
+    console.log(favoriteThings2);
+
+    const ul = document.createElement("ul");
+    const liOne = document.createElement("li");
+    const liTwo = document.createElement("li");
+    const liThree = document.createElement("li");
+
+    form.after(ul);
+    ul.append(liOne, liTwo, liThree);
+    liOne.append(favoriteThings2[0]); // favoriteThings2 = [second, first, third]
+    liTwo.append(favoriteThings2[1]);
+    liThree.append(favoriteThings2[2]);
+
+
   });
 });
